55166d29803fa9907875dc79326a300c8cc2bdbc
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-30 12:43:32 +0100

    supports different delimeters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,14 +1,27 @@
 class StringCalculator
 
+  def initialize
+    @sum = 0
+    @arr = []
+  end
+
   def add(num)
-    arr = num.split(",")
-    sum = 0
-    return 0 if (num == "")
+    return 0 if is_empty?(num)
+    return num.to_i if single_num?(num)
 
-    return num.to_i if arr.length == 1
-    arr.each { |x| sum += x.to_i }
-    return sum
+    @arr = num.gsub!((/[\D]/), " ").split(" ")
+    @arr.each { |x| @sum += x.to_i }
+    return @sum
+  end
 
+  private
+
+  def is_empty?(num)
+    num == ""
+  end
+
+  def single_num?(num)
+    num.length == 1
   end
 
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,4 +19,13 @@ describe StringCalculator do
   it "Returns the sum of unknown amount of numbers" do
     expect(calc.add("1,2,3,4,5,6,7")).to eq(28)
   end
+
+  it "Returns sum of numbers if new lines separate rather than commas" do
+    expect(calc.add("1\n2,3")).to eq(6)
+  end
+
+  it "Returns sum of numbers when different delimiters are used" do
+    expect(calc.add("//;\n1;2")).to eq(3)
+  end
+  
 end
